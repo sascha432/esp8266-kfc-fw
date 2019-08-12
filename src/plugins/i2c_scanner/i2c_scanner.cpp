3cce50e21b491659eda521578c4153e64e97a342
sascha lammers <sascha_lammers@gmx.de>
2019-08-12 00:47:22 -0700

    Reading LM75A and BME280, some improvements

--- src/plugins/i2c_scanner/i2c_scanner.cpp
+++ src/plugins/i2c_scanner/i2c_scanner.cpp
@@ -18,89 +18,97 @@
 #include "i2c_scanner.h"
 #include "plugins.h"
 
+#include "Adafruit_BME280.h"
+
 void check_if_exist_I2C(Print &output);
 
-uint8_t portArray[] = {16, 5, 4, 0, 2, 14, 12, 13};
-//String portMap[] = {"D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7"}; //for Wemos
-String portMap[] = {"GPIO16", "GPIO5", "GPIO4", "GPIO0", "GPIO2", "GPIO14", "GPIO12", "GPIO13"};
+uint8_t portArray[] = { 16, 5, 4, 0, 2, 14, 12, 13 };
+const char *portMap[] = { "GPIO16", "GPIO5", "GPIO4", "GPIO0", "GPIO2", "GPIO14", "GPIO12", "GPIO13" };
 
 void scanPorts(Print &output) {
-    output.println("\n\nI2C Scanner to scan for devices on each port pair D0 to D7");
+    output.println(F("\n\nI2C Scanner to scan for devices on each port pair D0 to D7"));
     for (uint8_t i = 0; i < sizeof(portArray); i++) {
         for (uint8_t j = 0; j < sizeof(portArray); j++) {
-            if (i != j){
-            output.print("Scanning (SDA : SCL) - " + portMap[i] + " : " + portMap[j] + " - ");
-            Wire.begin(portArray[i], portArray[j]);
-            check_if_exist_I2C(output);
+            if (i != j) {
+                output.printf_P(PSTR("Scanning (SDA : SCL) - %s : %s - "), portMap[i], portMap[j]);
+                Wire.begin(portArray[i], portArray[j]);
+                check_if_exist_I2C(output);
             }
         }
     }
 }
 
 void check_if_exist_I2C(Print &output) {
-  byte error, address;
-  int nDevices;
-  nDevices = 0;
-  for (address = 1; address < 127; address++ )  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
+    uint8_t error, address;
+    int nDevices;
 
-    if (error == 0){
-      output.print("I2C device found at address 0x");
-      if (address < 16)
-        output.print("0");
-      output.print(address, HEX);
-      output.println("  !");
+    nDevices = 0;
+    for (address = 1; address < 127; address++ )  {
 
-      nDevices++;
-    } else if (error == 4) {
-      output.print("Unknow error at address 0x");
-      if (address < 16)
-        output.print("0");
-      output.println(address, HEX);
+        // The i2c_scanner uses the return value of
+        // the Write.endTransmisstion to see if
+        // a device did acknowledge to the address.
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+
+        if (error == 0 || error == 3) { // 0=ACK, 3=NACK for data
+            output.printf_P(PSTR("I2C device found at address 0x%02x\n"), address);
+            nDevices++;
+        } 
+        else if (error == 4) {
+            output.printf_P(PSTR("Unknown error at address 0x%02x\n"), address);
+        }
+    } //for loop
+    if (nDevices == 0) {
+        output.println("No I2C devices found");
     }
-  } //for loop
-  if (nDevices == 0)
-    output.println("No I2C devices found");
-  else
-    output.println("**********************************\n");
-  //delay(1000);           // wait 1 seconds for next scan, did not find it necessary
+    else {
+        output.println(F("**********************************\n"));
+    }
+    //delay(1000);           // wait 1 seconds for next scan, did not find it necessary
 }
 
 #if AT_MODE_SUPPORTED
 
 #include "at_mode.h"
 
-PROGMEM_AT_MODE_HELP_COMMAND_DEF_PNPN(SCANI2C, "SCANI2C", "Scan for I2C bus and devices");
+PROGMEM_AT_MODE_HELP_COMMAND_DEF_PNPN(SCANI2C, "SCANI2C", "Scan for I2C bus and devices (might cause a crash)");
+PROGMEM_AT_MODE_HELP_COMMAND_DEF_PNPN(SCAND, "SCAND", "Scan for devices");
 PROGMEM_AT_MODE_HELP_COMMAND_DEF_PPPN(I2CS, "I2CS", "<SDA,SCL[,speed=100kHz[,clock strech limit=usec]]>", "Setup I2C bus");
-PROGMEM_AT_MODE_HELP_COMMAND_DEF_PPPN(I2CT, "I2CT", "<stop,address,byte[,byte][,byte,...]>", "Send data to slave");
+PROGMEM_AT_MODE_HELP_COMMAND_DEF_PPPN(I2CT, "I2CT", "<address,byte[,byte][,...]>", "Send data to slave (hex encoded: ff or 0xff)");
 PROGMEM_AT_MODE_HELP_COMMAND_DEF_PPPN(I2CR, "I2CR", "<address,count>", "Request data from slave");
+PROGMEM_AT_MODE_HELP_COMMAND_DEF_PPPN(I2CBME280, "I2CBME280", "<address>", "Read temperature, humidity and pressure from BME280");
+PROGMEM_AT_MODE_HELP_COMMAND_DEF_PPPN(I2CLM75A, "I2CLM75A", "<address>", "Read temperature from LM75A");
 
-static int __toint(const char *s) {
+static int __toint(const char *s, uint8_t base = 10) {
     if (*s == 'x') {
         return strtoul(s + 1, 0, 16);
     } else if (*s == '0' && *(s + 1) == 'x') {
         return strtoul(s + 2, 0, 16);
     } else {
-        return strtoul(s, 0, 16);
+        return strtoul(s, 0, base);
     }
 }
 
 void i2cscanner_setup() {
-    Wire.begin(4, 5);
-    Wire.setClockStretchLimit(15000);
+    Wire.begin(D3, D5);
+    Wire.setClockStretchLimit(45000);
+}
+
+void i2cscanner_device_error(Stream &output) {
+    output.println(F("+I2C: An error occured"));
 }
 
 bool i2cscanner_at_mode_command_handler(Stream &serial, const String &command, int8_t argc, char **argv) {
     if (command.length() == 0) {
 
         at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(SCANI2C));
+        at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(SCAND));
         at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(I2CS));
         at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(I2CT));
         at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(I2CR));
+        at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(I2CBME280));
+        at_mode_add_help(PROGMEM_AT_MODE_HELP_COMMAND_T(I2CLM75A));
 
     } 
     else if (constexpr_String_equalsIgnoreCase(command, PROGMEM_AT_MODE_HELP_COMMAND(I2CS))) {
@@ -129,13 +137,13 @@ bool i2cscanner_at_mode_command_handler(Stream &serial, const String &command, i
         }
         else {
             int tmp;
-            Wire.beginTransmission(tmp = __toint(argv[0]));
+            Wire.beginTransmission(tmp = __toint(argv[0], 16));
             serial.printf_P(PSTR("+I2CT: address=%02x"), tmp);
             if (argc > 1) {
                 serial.print(',');
             }
             for(uint8_t i = 1; i < argc; i++) {
-                tmp = __toint(argv[i]);
+                tmp = __toint(argv[i], 16);
                 Wire.write((uint8_t)tmp);
                 serial.printf_P(PSTR(" %02x"), tmp);
             }
@@ -149,8 +157,8 @@ bool i2cscanner_at_mode_command_handler(Stream &serial, const String &command, i
         }
         else {
             int address, count;
-            address = __toint(argv[0]);
-            count = __toint(argv[1]);
+            address = __toint(argv[0], 16);
+            count = __toint(argv[1], 16);
             serial.printf_P(PSTR("+I2CR: address=%02x, count=%d, "), address, count);
             auto result = Wire.requestFrom(address, count);
             serial.printf_P(PSTR("result=%u, expected=%u, "), result, count);
@@ -176,6 +184,44 @@ bool i2cscanner_at_mode_command_handler(Stream &serial, const String &command, i
     } 
     else if (constexpr_String_equalsIgnoreCase(command, PROGMEM_AT_MODE_HELP_COMMAND(SCANI2C))) {
         scanPorts(serial);
+        return true;
+    }
+    else if (constexpr_String_equalsIgnoreCase(command, PROGMEM_AT_MODE_HELP_COMMAND(SCAND))) {
+        serial.println(F("Scanning I2C bus for devices..."));
+        check_if_exist_I2C(serial);
+        return true;
+    }
+    else if (constexpr_String_equalsIgnoreCase(command, PROGMEM_AT_MODE_HELP_COMMAND(I2CBME280))) {
+        if (argc != 1) {
+            at_mode_print_invalid_arguments(serial);
+        }
+        else {
+            Adafruit_BME280 bme280;
+            int address = __toint(argv[0]);
+            bme280.begin(address, &Wire);
+            serial.printf_P(PSTR("Reading BME280 at 0x%02x: %.2f °C, %.2f%%, %.2f hPa\n"), address, bme280.readTemperature(), bme280.readHumidity(), bme280.readPressure() / 100.0);
+        }
+        return true;
+    }
+    else if (constexpr_String_equalsIgnoreCase(command, PROGMEM_AT_MODE_HELP_COMMAND(I2CLM75A))) {
+        if (argc != 1) {
+            at_mode_print_invalid_arguments(serial);
+        }
+        else {
+            int address;
+            address = __toint(argv[0]);
+            serial.printf_P(PSTR("Reading LM75A at 0x%02x: "), address);
+            Wire.beginTransmission(address);
+            Wire.write(0x00);
+            if (Wire.endTransmission() == 0 && Wire.requestFrom(address, 2) == 2) {
+                auto temp = (((uint8_t)Wire.read() << 8) | (uint8_t)Wire.read()) / 256.0;
+                serial.printf_P(PSTR("%.2f °C\n"), temp);
+            } 
+            else {
+                serial.println();
+                i2cscanner_device_error(serial);
+            }
+        }
         return true;
     }
     return false;

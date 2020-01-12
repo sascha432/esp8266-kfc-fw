d83bd8f5d09f3bf98807b2512ef5951db8ba3141
sascha <sascha_lammers@gmx.de>
2020-01-11 22:30:23 -0800

    OTA tool check alive, status and export settings option

--- scripts/tools/kfcfw_ota.py
+++ scripts/tools/kfcfw_ota.py
@@ -37,7 +37,31 @@ def get_h3(content):
     except:
         return None
 
-def display_status(resp):
+
+def is_alive(url, target):
+    verbose("Checking if device is alive "  + target)
+    try:
+        resp = requests.get(url + "is_alive", timeout=1)
+        if resp.status_code!=200:
+            return False
+        elif resp.content.decode()!='0':
+            return False
+        return True
+    except:
+        return False
+
+def get_alive(url, target):
+    verbose("Checking if device is alive "  + target)
+    resp = requests.get(url + "is_alive", timeout=30)
+    if resp.status_code!=200:
+        error("Device did not respond", 2)
+    elif resp.content.decode()!='0':
+        error("Invalid response", 2)
+    verbose("Device responded")
+
+def get_status(url, target, sid):
+    verbose("Querying status "  + target)
+    resp = requests.get(url + "status.html", data={"SID": sid}, timeout=30)
     if resp.status_code==200:
         content = resp.content.decode()
         software = get_div("Software", content)
@@ -47,19 +71,89 @@ def display_status(resp):
         if software!=None:
             verbose("Hardware: " + hardware)
     elif resp.status_code==403:
-        error("Access denied")
+        error("Access denied", 2)
     elif resp.status_code==404:
-        error("Status page not found")
+        error("Status page not found", 2)
     else:
-        error("Invalid response code " + str(resp.status_code))
+        error("Invalid response code " + str(resp.status_code), 2)
 
-parser = argparse.ArgumentParser(description="OTA for KFC Firmware", formatter_class=argparse.RawDescriptionHelpFormatter, epilog="exit codes:\n  0 - success\n  1 - general error\n  2 - update failed\n  3 - device did not respond after update")
+def flash(url, type, target, sid):
+
+    image_type = "u_" + type
+
+    is_alive(url, target)
+    if args.no_status==False:
+        get_status(url, target, sid)
+
+    verbose("Uploading " + str(os.fstat(args.image.fileno()).st_size) + " Bytes: " + args.image.name)
+
+    resp = requests.post(url + "update", files={"firmware_image": args.image}, data={"image_type": image_type, "SID": sid}, timeout=30, allow_redirects=False)
+    if resp.status_code==302:
+        verbose("Update successful")
+    else:
+        content = resp.content.decode()
+        error = get_h3(content)
+        if error==None:
+            verbose("Update failed with unknown response")
+            verbose("Response code " + str(resp.status_code))
+            verbose(content)
+            sys.exit(2)
+        else:
+            verbose("Update failed with: " + error)
+            sys.exit(3)
+
+    if args.no_wait==False:
+        verbose("Waiting for device to reboot", False)
+        if args.quiet==False:
+            for i in range(0, 2):
+                time.sleep(3)
+                print(".", end="", flush=True)
+        else:
+            time.sleep(4)
+
+        max_wait = 60
+        while True:
+            if args.quiet==False:
+                print(".", end="", flush=True)
+            if is_alive(url, target):
+                if args.no_status==False:
+                    get_status(url, target, sid)
+                break
+            time.sleep(1)
+            max_wait = max_wait - 1
+            if max_wait==0:
+                error("Device did not response after update", 4)
+
+def export_settings(url, target, sid, output):
+
+    if is_alive(url, target):
+        resp = requests.get(url + 'export_settings', data={"SID": sid})
+        if resp.status_code==200:
+            if resp.headers['Content-Type']!='application/json':
+                error("Content type not JSON")
+            else:
+                content = resp.content.decode()
+                if output==None:
+                    print(content)
+                else:
+                    with open(output, "wt") as file:
+                        file.write(content)
+                    verbose("Settings exported: " + output)
+        else:
+            error("Invalid response status code " + str(resp.status_code))
+    else:
+        error("Device did not respond")
+
+# main
+
+parser = argparse.ArgumentParser(description="OTA for KFC Firmware", formatter_class=argparse.RawDescriptionHelpFormatter, epilog="exit codes:\n  0 - success\n  1 - general error\n  2 - device did not respond\n  3 - update failed\n  4 - device did not respond after update")
+parser.add_argument("action", help="action to execute", choices=["flash", "spiffs", "atmega", "status", "alive", "export"])
 parser.add_argument("hostname", help="web server hostname")
-parser.add_argument("image", help="firmware image", type=argparse.FileType("rb"))
 parser.add_argument("-u", "--user", help="Username", required=True)
 parser.add_argument("-p", "--pw", help="password", required=True)
+parser.add_argument("-I", "--image", help="firmware image", type=argparse.FileType("rb"))
+parser.add_argument("-O", "--output", help="export settings file")
 parser.add_argument("-P", "--port", help="web server port", type=int, default=80)
-parser.add_argument("-t", "--type", help="type of image ", choices=["firmware", "spiffs", "atmega"], default="firmware")
 parser.add_argument("-s", "--secure", help="use https to upload", action="store_true", default=False)
 parser.add_argument("-q", "--quiet", help="do not show any output", action="store_true", default=False)
 parser.add_argument("-n", "--no-status", help="do not query status", action="store_true", default=False)
@@ -71,69 +165,14 @@ if args.secure:
     url += "s"
 url = url + "://" + args.hostname + "/"
 
-# //firmware_image
-image_type = "u_flash"
-if args.type!="firmware":
-    image_type = "u_" + image_type
-
 sid = kfcfw_session.Session().generate(args.user, args.pw)
+target = args.user + ":***@" + args.hostname
 
-if args.no_status:
-    verbose("Checking if device is alive "  + args.user + ":***@" + args.hostname)
-    resp = requests.get(url + "is_alive", timeout=30)
-    if resp.status_code!=200 or resp.content.decode()!='0':
-        error("Device did not respond")
-else:
-    verbose("Querying status "  + args.user + ":***@" + args.hostname)
-    resp = requests.get(url + "status.html", data={"SID": sid}, timeout=30)
-    display_status(resp)
-
-verbose("Uploading " + str(os.fstat(args.image.fileno()).st_size) + " Bytes: " + args.image.name)
-
-resp = requests.post(url + "update", files={"firmware_image": args.image}, data={"image_type": image_type, "SID": sid}, timeout=30, allow_redirects=False)
-if resp.status_code==302:
-    verbose("Update successful")
-else:
-    content = resp.content.decode()
-    error = get_h3(content)
-    if error==None:
-        verbose("Update failed with unknown response")
-        verbose("Response code " + str(resp.status_code))
-        verbose(content)
-        sys.exit(2)
-    else:
-        verbose("Update failed with: " + error)
-        sys.exit(2)
-
-if args.no_wait==False:
-    verbose("Waiting for device to reboot", False)
-    if args.quiet==False:
-        for i in range(0, 2):
-            time.sleep(2)
-            print(".", end="", flush=True)
-    else:
-        time.sleep(4)
-
-    max_wait = 60
-    while True:
-        if args.quiet==False:
-            print(".", end="", flush=True)
-        try:
-            if args.no_status:
-                resp = requests.get(url + "is_alive", timeout=1)
-                if resp.status_code==200 and resp.content.decode()=='0':
-                    verbose("")
-                    break
-            else:
-                resp = requests.get(url + "status.html", data={"SID": sid}, timeout=1)
-                if resp.status_code==200:
-                    verbose("")
-                    display_status(resp)
-                    break
-        except:
-            time.sleep(1)
-            max_wait = max_wait - 1
-            if max_wait==0:
-                error("Device did not response after update", 3)
-        else:
-            break
+if args.action in("flash", "spiffs", "atmega"):
+    flash(url, args.action, target, sid)
+elif args.action=="status":
+    get_status(url, target, sid)
+elif args.action=="alive":
+    get_alive(url, target)
+elif args.action=="export":
+    export_settings(url, target, sid, args.output)

23e74de8432984cf068d5cf3e477bc928e3765de
sascha <sascha_lammers@gmx.de>
2020-02-11 03:17:50 -0800

    Generic buttons plugin

---
+++ src/plugins/buttons/buttons.cpp
@@ -0,0 +1,104 @@
+/**
+ * Author: sascha_lammers@gmx.de
+ */
+
+#if IOT_BUTTONS
+
+#include <LoopFunctions.h>
+#include <KFCForms.h>
+#include "buttons.h"
+
+static ButtonsPlugin plugin;
+
+void ButtonsPlugin::setup(PluginSetupMode_t mode)
+{
+    _readConfig();
+    PinMonitor &monitor = *PinMonitor::createInstance();
+    for(auto &button: _buttons) {
+        auto pin = monitor.addPin(button.getPin(), pinCallback, this, button.getPinMode());
+        if (pin) {
+            _debug_printf_P(PSTR("pin=%u added\n"), button.getPin());
+            PushButton &pushButton = button.getButton();
+            pushButton.onPress(onButtonPressed);
+            pushButton.onHoldRepeat(button.getConfig().longpress.time, button.getConfig().repeat.time, onButtonHeld);
+            pushButton.onRelease(onButtonReleased);
+
+        }
+        else {
+            _debug_printf_P(PSTR("failed to add pin=%u\n"), button.getPin());
+            monitor.removePin(button.getPin(), this);
+        }
+    }
+}
+
+void ButtonsPlugin::restart()
+{
+    PinMonitor *monitor = PinMonitor::getInstance();
+    if (monitor) {
+        for(auto &button: _buttons) {
+            monitor->removePin(button.getPin(), this);
+        }
+        if (!monitor->size()) {
+            PinMonitor::deleteInstance();
+        }
+    }
+    LoopFunctions::remove(loop);
+}
+
+void ButtonsPlugin::reconfigure(PGM_P source)
+{
+    PinMonitor *monitor = PinMonitor::getInstance();
+    if (monitor) {
+        for(auto &button: _buttons) {
+            monitor->removePin(button.getPin(), this);
+        }
+    }
+    setup(PLUGIN_SETUP_DEFAULT);
+    if (!monitor->size()) {
+        PinMonitor::deleteInstance();
+    }
+}
+
+void ButtonsPlugin::createConfigureForm(AsyncWebServerRequest *request, Form &form)
+{
+    form.finalize();
+}
+
+void ButtonsPlugin::_readConfig()
+{
+    _buttons.clear();
+    Config_Button::ButtonVector buttons;
+    Config_Button::getButtons(buttons);
+    for(auto &button: buttons) {
+        _debug_printf_P(PSTR("pin=%u\n"), button.pin);
+        _buttons.emplace_back(button);
+    }
+    _debug_printf_P(PSTR("size=%u\n"), _buttons.size());
+}
+
+void ButtonsPlugin::pinCallback(void *arg)
+{
+
+}
+
+void ButtonsPlugin::loop() {
+
+}
+
+void ButtonsPlugin::onButtonPressed(::Button& btn)
+{
+
+}
+
+void ButtonsPlugin::onButtonHeld(::Button& btn, uint16_t duration, uint16_t repeatCount)
+{
+
+}
+
+void ButtonsPlugin::onButtonReleased(::Button& btn, uint16_t duration)
+{
+
+}
+
+
+#endif
---
+++ src/plugins/buttons/buttons.h
@@ -0,0 +1,106 @@
+/**
+ * Author: sascha_lammers@gmx.de
+ */
+
+#if IOT_BUTTONS
+
+#pragma once
+
+#include <Arduino_compat.h>
+#include <Button.h>
+#include <ButtonEventCallback.h>
+#include <PushButton.h>
+#include <Bounce2.h>
+#include <vector>
+#include "plugins.h"
+#include "pin_monitor.h"
+#include "kfc_fw_config.h"
+
+#ifndef DEBUG_IOT_BUTTONS
+#define DEBUG_IOT_BUTTONS                               1
+#endif
+
+#define MAX_BUTTONS                                     8
+
+#if !defined(PIN_MONITOR) || !PIN_MONITOR
+#error requires PIN_MONITOR=1
+#endif
+
+class ButtonsPlugin : public PluginComponent {
+public:
+    typedef Config_Button::Button_t Button_t;
+
+    class Button {
+    public:
+        Button(const Button_t &config) : _config(config), _button(PushButton(_config.pin, _config.pinMode)) {
+        }
+        ~Button() {
+        }
+        uint8_t getPin() const  {
+            return _config.pin;
+        }
+        uint8_t getPinMode() const  {
+            return _config.pinMode;
+        }
+        Button_t &getConfig() {
+            return _config;
+        }
+        PushButton &getButton() {
+            return _button;
+        }
+    private:
+        Button_t _config;
+        PushButton _button;
+    };
+
+    typedef std::vector<Button> ButtonVector;
+
+public:
+    ButtonsPlugin() {
+        REGISTER_PLUGIN(this);
+    }
+
+    virtual PGM_P getName() const {
+        return PSTR("buttons");
+    }
+
+    virtual PluginPriorityEnum_t getSetupPriority() const {
+        return PRIO_BUTTONS;
+    }
+    virtual bool autoSetupAfterDeepSleep() const override {
+        return true;
+    }
+
+    virtual void setup(PluginSetupMode_t mode) override;
+    virtual void restart() override;
+    virtual void reconfigure(PGM_P source) override;
+
+    virtual MenuTypeEnum_t getMenuType() const override {
+        return AUTO;
+    }
+    // virtual void createMenu() override {
+    //     bootstrapMenu.addSubMenu(F("Buttons"), F("buttons.html"), navMenu.config);
+    // }
+
+    virtual PGM_P getConfigureForm() const override {
+        return getName();
+    }
+    virtual void createConfigureForm(AsyncWebServerRequest *request, Form &form) override;
+
+
+public:
+    static void pinCallback(void *arg);
+    static void loop();
+
+    static void onButtonPressed(::Button& btn);
+    static void onButtonHeld(::Button& btn, uint16_t duration, uint16_t repeatCount);
+    static void onButtonReleased(::Button& btn, uint16_t duration);
+
+private:
+    void _readConfig();
+
+private:
+    ButtonVector _buttons;
+};
+
+#endif

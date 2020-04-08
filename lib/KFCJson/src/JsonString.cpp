685b82ec60e0ff54e5c0c02f66a8a4aaf61c806a
sascha <sascha_lammers@gmx.de>
2020-04-08 08:34:10 -0700

    Fixed move and copy constructor

--- lib/KFCJson/src/JsonString.cpp
+++ lib/KFCJson/src/JsonString.cpp
@@ -4,28 +4,34 @@
 
 #include "JsonString.h"
 
-//JsonString::JsonString(const JsonString &str) {
-//    *this = str;
-//}
-//
-//JsonString::JsonString(JsonString && str) {
-//    *this = std::move(str);
-//}
-//
-JsonString::JsonString() {
+JsonString::JsonString(const JsonString &str) : JsonString()
+{
+   *this = str;
+}
+
+JsonString::JsonString(JsonString &&str) : JsonString()
+{
+   *this = std::move(str);
+}
+
+JsonString::JsonString()
+{
     _setType(STORED);
     *_raw = 0;
 }
 
-JsonString::JsonString(const String & str) {
+JsonString::JsonString(const String &str)
+{
     _init(str.c_str(), (length_t)str.length());
 }
 
-JsonString::JsonString(const char * str, bool forceCopy) {
+JsonString::JsonString(const char *str, bool forceCopy)
+{
     _init(str, (length_t)strlen(str));
 }
 
-JsonString::JsonString(const char * str) {
+JsonString::JsonString(const char *str)
+{
     if (*str) {
         _setType(POINTER);
         _setPtr(str);
@@ -37,7 +43,8 @@ JsonString::JsonString(const char * str) {
     }
 }
 
-JsonString::JsonString(const __FlashStringHelper * str) {
+JsonString::JsonString(const __FlashStringHelper *str)
+{
     // while checking string length we can copy already from slow flash
     PGM_P lptr = RFPSTR(str);
     auto dptr = _raw;
@@ -59,19 +66,22 @@ JsonString::JsonString(const __FlashStringHelper * str) {
     }
 }
 
-JsonString::JsonString(const __FlashStringHelper * str, bool forceFlash) {
+JsonString::JsonString(const __FlashStringHelper * str, bool forceFlash)
+{
     _setType(FLASH);
     _setPtr(reinterpret_cast<const char *>(str));
     _setLength((length_t)strlen_P(RFPSTR(str)));
 }
 
-JsonString::~JsonString() {
+JsonString::~JsonString()
+{
     if (_getType() == ALLOC) {
         free(_getPtr());
     }
 }
 
-JsonString & JsonString::operator=(const JsonString & str) {
+JsonString &JsonString::operator=(const JsonString &str)
+{
     if (_getType() == ALLOC) {
         free(_getPtr());
     }
@@ -84,7 +94,8 @@ JsonString & JsonString::operator=(const JsonString & str) {
     return *this;
 }
 
-JsonString & JsonString::operator=(JsonString && str) {
+JsonString & JsonString::operator=(JsonString &&str)
+{
     if (_getType() == ALLOC) {
         free(_getPtr());
     }
@@ -101,37 +112,44 @@ JsonString & JsonString::operator=(JsonString && str) {
     return *this;
 }
 
-bool JsonString::operator==(const char * str) const {
+bool JsonString::operator==(const char *str) const
+{
     return equals(str);
 }
 
-bool JsonString::operator==(const String & str) const {
+bool JsonString::operator==(const String &str) const
+{
     return equals(str);
 }
 
-bool JsonString::operator==(const JsonString & str) const {
+bool JsonString::operator==(const JsonString &str) const
+{
     return equals(str);
 }
 
-bool JsonString::operator==(const __FlashStringHelper * str) const {
+bool JsonString::operator==(const __FlashStringHelper *str) const
+{
     return equals(str);
 }
 
-bool JsonString::equals(const char * str) const {
+bool JsonString::equals(const char *str) const
+{
     if (isProgMem()) {
         return strcmp_P(str, _getConstPtr()) == 0;
     }
     return strcmp(str, getPtr()) == 0;
 }
 
-bool JsonString::equals(const String & str) const {
+bool JsonString::equals(const String &str) const
+{
     if (isProgMem()) {
         return strcmp_P(str.c_str(), getPtr()) == 0;
     }
     return strcmp(str.c_str(), getPtr()) == 0;
 }
 
-bool JsonString::equals(const JsonString & str) const {
+bool JsonString::equals(const JsonString &str) const
+{
     if (isProgMem()) {
         if (str.isProgMem()) {
             return strcmp_P_P(str.getPtr(), getPtr()) == 0;
@@ -143,14 +161,16 @@ bool JsonString::equals(const JsonString & str) const {
     return strcmp(str.getPtr(), getPtr()) == 0;
 }
 
-bool JsonString::equals(const __FlashStringHelper *str) const {
+bool JsonString::equals(const __FlashStringHelper *str) const
+{
     if (isProgMem()) {
         return strcmp_P_P(getPtr(), RFPSTR(str)) == 0;
     }
     return strcmp_P(getPtr(), RFPSTR(str)) == 0;
 }
 
-void JsonString::clear() {
+void JsonString::clear()
+{
     if (_getType() == ALLOC) {
         free(_getPtr());
     }
@@ -158,14 +178,16 @@ void JsonString::clear() {
     *_raw = 0;
 }
 
-size_t JsonString::length() const {
+size_t JsonString::length() const
+{
     if (_getType() == STORED) {
         return strlen(_raw);
     }
     return _getLength();
 }
 
-String JsonString::toString() const {
+String JsonString::toString() const
+{
     if (_getType() == FLASH) {
         return String(FPSTR(_getConstPtr()));
     }
@@ -175,18 +197,21 @@ String JsonString::toString() const {
     return String(_getConstPtr());
 }
 
-bool JsonString::isProgMem() const {
+bool JsonString::isProgMem() const
+{
     return _getType() == FLASH;
 }
 
-const char * JsonString::getPtr() const {
+const char *JsonString::getPtr() const
+{
     if (_getType() == STORED) {
         return _raw;
     }
     return _getConstPtr();
 }
 
-size_t JsonString::printTo(Print & p) const {
+size_t JsonString::printTo(Print &p) const
+{
     if (_getType() == FLASH) {
         return p.print(FPSTR(_getConstPtr()));
     }
@@ -196,14 +221,16 @@ size_t JsonString::printTo(Print & p) const {
     return p.write(_getConstPtr(), _getLength());
 }
 
-char * JsonString::_allocate(length_t length) {
+char *JsonString::_allocate(length_t length)
+{
     _setType(ALLOC);
     auto ptr = (char *)malloc(length + 1);
     _setPtr(ptr);
     return ptr;
 }
 
-void JsonString::_init(const char * str, length_t length) {
+void JsonString::_init(const char *str, length_t length)
+{
     if (length < buffer_size) {
         _setType(STORED);
         strncpy(_raw, str, length)[length] = 0;

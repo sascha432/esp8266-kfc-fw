dc41992a042b140a8ee030e5318489aab748ea77
sascha <sascha_lammers@gmx.de>
2020-04-22 03:46:07 -0700

    EventScheduler implementation for Win32

--- lib-mock/KFCBaseLibrary/include/EventScheduler.h
+++ lib-mock/KFCBaseLibrary/include/EventScheduler.h
@@ -6,139 +6,138 @@
 
 #include <Arduino_compat.h>
 
- class EventTimer {
- public:
-     void detach() {}
- };
+class EventTimer;
+class EventScheduler;
 
- class EventTimer;
- class EventScheduler;
+extern EventScheduler Scheduler;
 
- extern EventScheduler Scheduler;
+class EventScheduler {
+public:
+    class RepeatType {
+    public:
+        static const int UNLIMITED = -1;
+        static const int YES = 1;
+        static const int NO = 0;
 
- class EventScheduler {
- public:
-     class RepeatType {
-     public:
-         static const int UNLIMITED = -1;
-         static const int YES = 1;
-         static const int NO = 0;
+        // true for UNLIMITED or false for no repeat
+        RepeatType(bool repeat = false) : _counter(0), _maxRepeat(repeat ? UNLIMITED : NO) {
+        }
+        // number of repeats, YES for calling it once, UNLIMITED or 0
+        RepeatType(int repeat) : _counter(0), _maxRepeat(repeat) {
+        }
 
-         // true for UNLIMITED or false for no repeat
-         RepeatType(bool repeat = false) : _counter(0), _maxRepeat(repeat ? UNLIMITED : NO) {
-         }
-         // number of repeats, YES for calling it once, UNLIMITED or 0
-         RepeatType(int repeat) : _counter(0), _maxRepeat(repeat) {
-         }
+    protected:
+        friend EventScheduler;
+        friend EventTimer;
 
-     protected:
-         friend EventScheduler;
-         friend EventTimer;
+        inline bool hasRepeat() const {
+            return (_maxRepeat == UNLIMITED) || (_counter <= _maxRepeat);
+        }
 
-         inline bool hasRepeat() const {
-             return (_maxRepeat == UNLIMITED) || (_counter <= _maxRepeat);
-         }
+        int _counter;
+        int _maxRepeat;
+    };
 
-         int _counter;
-         int _maxRepeat;
-     };
+    class RepeatUpdateType : public RepeatType {
+    public:
+        using RepeatType::RepeatType;
 
-     class RepeatUpdateType : public RepeatType {
-     public:
-         using RepeatType::RepeatType;
+        static const int NO_CHANGE = -2;
 
-         static const int NO_CHANGE = -2;
+        // keep repeat
+        RepeatUpdateType() : RepeatType(NO_CHANGE) {
+        }
 
-         // keep repeat
-         RepeatUpdateType() : RepeatType(NO_CHANGE) {
-         }
+    private:
+        friend EventTimer;
 
-     private:
-         friend EventTimer;
+        inline bool isUpdateRequired() {
+            return _maxRepeat != NO_CHANGE;
+        }
+    };
 
-         inline bool isUpdateRequired() {
-             return _maxRepeat != NO_CHANGE;
-         }
-     };
+    typedef enum {
+        PRIO_NONE = -1,
+        PRIO_LOW = 0,
+        PRIO_NORMAL,                // below HIGH, the timer callback is executed in the main loop and might be considerably delayed
+        PRIO_HIGH,                  // HIGH runs the timer callback directly inside the timer interrupt/task without any delay
+    } Priority_t;
 
-     typedef enum {
-         PRIO_NONE = -1,
-         PRIO_LOW = 0,
-         PRIO_NORMAL,                // below HIGH, the timer callback is executed in the main loop and might be considerably delayed
-         PRIO_HIGH,                  // HIGH runs the timer callback directly inside the timer interrupt/task without any delay
-     } Priority_t;
+    typedef std::shared_ptr<EventTimer> TimerPtr;
+    typedef std::function<void(TimerPtr timer)> Callback;
+    typedef std::function<void(EventTimer *)> DeleterCallback;
+    typedef std::vector<TimerPtr> TimerVector;
+    typedef std::vector<TimerPtr>::iterator TimerIterator;
 
-     typedef std::shared_ptr<EventTimer> TimerPtr;
-     typedef std::function<void(TimerPtr timer)> Callback;
-     typedef std::function<void(EventTimer *)> DeleterCallback;
-     typedef std::vector<TimerPtr> TimerVector;
-     typedef std::vector<TimerPtr>::iterator TimerIterator;
+public:
+    class Timer {
+    public:
+        Timer() {}
+        ~Timer() {
+            remove();
+        }
 
- public:
-     class Timer {
-     public:
-         Timer() {}
-         ~Timer() {}
+        Timer(const Timer &timer) = delete;
+        Timer(Timer &&timer) {
+            *this = std::move(timer);
+        }
 
-         Timer(const Timer &timer) = delete;
-         Timer(Timer &&timer);
+        Timer &operator=(Timer &&timer)
+        {
+            //debug_printf_P(PSTR("_timer=%p.swap(%p)\n"), _timer.get(), timer._timer.get());
+            _timer = timer._timer;
+            timer._timer = nullptr;
+            return *this;
+        }
 
-         Timer &operator=(Timer &&timer)
-         {
-             //debug_printf_P(PSTR("_timer=%p.swap(%p)\n"), _timer.get(), timer._timer.get());
-             _timer = timer._timer;
-             timer._timer = nullptr;
-             return *this;
-         }
+        void add(int64_t delayMillis, RepeatType repeat, Callback callback, Priority_t priority = PRIO_LOW, DeleterCallback deleter = nullptr);
+        bool remove();
 
-         void add(int64_t delayMillis, RepeatType repeat, Callback callback, Priority_t priority = PRIO_LOW, DeleterCallback deleter = nullptr) {
+        bool active() const;
 
-         }
-         bool remove() {
-             return  true;
-         }
+        EventTimer *operator->() const {
+            return _timer;
+        }
 
-         inline bool active() const {
-             return _timer != nullptr;
-         }
+    private:
+        EventTimer *_timer;
+    };
 
-         EventTimer *operator->() const;
+public:
+    EventScheduler() : _runtimeLimit(250) {
+    }
 
-     private:
-         EventTimer *_timer;
-     };
+    void begin();
+    void end();
 
- public:
-     EventScheduler() : _runtimeLimit(250) {
-     }
+    EventTimer *addTimer(int64_t delayMillis, RepeatType repeat, Callback callback, Priority_t priority = PRIO_LOW, DeleterCallback deleter = nullptr);
 
-     void begin();
-     void end();
+    // checks if "timer" exists and is running
+    bool hasTimer(EventTimer *timer) const;
 
-     EventTimer *addTimer(int64_t delayMillis, RepeatType repeat, Callback callback, Priority_t priority = PRIO_LOW, DeleterCallback deleter = nullptr);
+    // remove timer and return true if it has been removed
+    bool removeTimer(EventTimer *timer);
 
-     // checks if "timer" exists and is running
-     bool hasTimer(EventTimer *timer) const;
+    // returns number of scheduled timers
+    size_t size() const {
+        return _timers.size();
+    }
 
-     // remove timer and return true if it has been removed
-     bool removeTimer(EventTimer *timer);
+public:
+    static void loop();
+    static void _timerCallback(void *arg);
 
-     // returns number of scheduled timers
-     size_t size() const {
-         return _timers.size();
-     }
+private:
+#if DEBUG_EVENT_SCHEDULER
+    void _list();
+#endif
+    friend EventTimer;
+    friend void at_mode_list_ets_timers(Print &output);
 
- public:
-     static void loop();
-     static void _timerCallback(void *arg);
+    void _removeTimer(EventTimer *timer);
+    void _loop();
 
- private:
-     friend EventTimer;
-
-     void _removeTimer(EventTimer *timer);
-     void _loop();
-
- private:
-     TimerVector _timers;
-     int _runtimeLimit; // if a callback takes longer than this amount of time, the next callback will be delayed until the next loop function call
- };
+private:
+    TimerVector _timers;
+    int _runtimeLimit; // if a callback takes longer than this amount of time, the next callback will be delayed until the next loop function call
+};
---
+++ lib-mock/KFCBaseLibrary/src/EventScheduler.cpp
@@ -0,0 +1,65 @@
+/**
+  Author: sascha_lammers@gmx.de
+*/
+
+#include "EventScheduler.h"
+#include "EventTimer.h"
+#include <thread>
+
+EventScheduler Scheduler;
+
+EventTimer *EventScheduler::addTimer(int64_t delayMillis, RepeatType repeat, Callback callback, Priority_t priority, DeleterCallback deleter)
+{
+    auto timer = new EventTimer(callback, delayMillis, repeat, priority);
+    _timers.push_back(deleter ? TimerPtr(timer, deleter) : TimerPtr(timer));
+    timer->initTimer();
+    return timer;
+}
+
+bool EventScheduler::hasTimer(EventTimer *timer) const
+{
+    if (timer) {
+        for (const auto &_timer : _timers) {
+            if (_timer.get() == timer) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool EventScheduler::removeTimer(EventTimer *timer)
+{
+    return false;
+}
+
+void EventScheduler::Timer::add(int64_t delayMillis, RepeatType repeat, Callback callback, Priority_t priority, DeleterCallback deleter)
+{
+    if (_timer) {
+        remove();
+    }
+    _timer = Scheduler.addTimer(delayMillis, repeat, callback, priority, [this, deleter](EventTimer *timer) {
+        if (deleter) {
+            deleter(timer);
+        }
+        else {
+            delete timer;
+        }
+        _timer = nullptr;
+    });
+}
+
+bool EventScheduler::Timer::remove()
+{
+    if (_timer) {
+        _timer->_remove();
+        _timer = nullptr;
+        return true;
+    }
+    return false;
+}
+
+bool EventScheduler::Timer::active() const 
+{
+    return _timer && _timer->active();
+}

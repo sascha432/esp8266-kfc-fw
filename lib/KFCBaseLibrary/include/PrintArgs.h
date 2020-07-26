5e6dc86d84856f82ab29c5846b16edd1efc737a9
sascha <sascha_lammers@gmx.de>
2020-07-25 20:29:26 -0700

    attach temporary strrings with deduplication

--- lib/KFCBaseLibrary/include/PrintArgs.h
+++ lib/KFCBaseLibrary/include/PrintArgs.h
@@ -15,8 +15,10 @@
 
 #if DEBUG_PRINT_ARGS
 #include "debug_helper_enable.h"
+#define DEBUG_CHECK_DUPES                   1
 #else
 #include "debug_helper_disable.h"
+#define DEBUG_CHECK_DUPES                   1
 #endif
 
 // adds streaming capabilities to Print objects
@@ -38,6 +40,13 @@ public:
 #endif
     }
     ~PrintArgs() {
+#if DEBUG_PRINT_ARGS
+        __DBG_print("--attached---");
+        for(const auto &str: _attached) {
+            __DBG_print(str);
+        }
+        __DBG_print("--end---");
+#endif
     }
 
     void clear();
@@ -50,10 +59,55 @@ public:
         }
         else {
             _buffer.write(sizeof...(args));
-            //_debug_printf_P(PSTR("this=%p buffer=%d\n"), this, _buffer.size());
             _store((size_t)format);
             _collect(args...);
         }
+    }
+
+public:
+    // returns nullptr if the string is not attach
+    // otherwise it returns the string or a string with the same content
+    const char *getAttachedString(const char *cStr) const {
+        auto iterator = std::find(_attached.begin(), _attached.end(), cStr);
+        if (iterator != _attached.end()) {
+            return iterator->c_str();
+        }
+        return nullptr;
+    }
+
+    // attach a temporary string to the output object
+    const char *attachString(String &&str) {
+#if DEBUG_CHECK_DUPES
+        auto iterator = std::find(_attached.begin(), _attached.end(), str);
+        if (iterator != _attached.end()) {
+            __DBG_printf("duplicate string='%s'", iterator->c_str());
+        }
+#endif
+        _attached.emplace_back(std::move(str));
+        return _attached.back().c_str();
+    }
+    const char *attachConstString(const String &str) {
+        return attachConstString(str.c_str());
+    }
+    const char *attachConstString(const __FlashStringHelper *fstr) {
+#if DEBUG_CHECK_DUPES
+        auto iterator = std::find(_attached.begin(), _attached.end(), fstr);
+        if (iterator != _attached.end()) {
+            __DBG_printf("duplicate string='%s'", iterator->c_str());
+        }
+#endif
+        _attached.emplace_back(fstr);
+        return _attached.back().c_str();
+    }
+    const char *attachConstString(const char *cstr) {
+#if DEBUG_CHECK_DUPES
+        auto iterator = std::find(_attached.begin(), _attached.end(), cstr);
+        if (iterator != _attached.end()) {
+            __DBG_printf("duplicate string='%s'", iterator->c_str());
+        }
+#endif
+        _attached.emplace_back(cstr);
+        return _attached.back().c_str();
     }
 
 private:
@@ -79,6 +133,7 @@ private:
 protected:
     static const int PrintObject = -1;
 
+    StringVector _attached;
     uint8_t *_bufferPtr;
     int _position;
 private:

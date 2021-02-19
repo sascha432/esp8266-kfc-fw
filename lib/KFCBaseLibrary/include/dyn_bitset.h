8eab6ad7417424778b46356a0cfaf148f57bae6e
sascha <sascha_lammers@gmx.de>
2021-02-19 00:33:01 -0800

    fixed potential memory leak and freeing of uninitialized pointer

--- lib/KFCBaseLibrary/include/dyn_bitset.h
+++ lib/KFCBaseLibrary/include/dyn_bitset.h
@@ -9,9 +9,11 @@
 
 class dynamic_bitset {
 private:
+
     class bit_helper {
     private:
         friend dynamic_bitset;
+
         bit_helper(dynamic_bitset *bitset, uint8_t index);
 
     public:
@@ -42,28 +44,25 @@ public:
     dynamic_bitset(const char *fromBytes, uint8_t length, uint8_t size);
     ~dynamic_bitset();
 
-    dynamic_bitset(const dynamic_bitset &bits) {
-        *this = bits;
-    }
-    dynamic_bitset(dynamic_bitset &&bits) {
-        *this = std::move(bits);
-    }
+    dynamic_bitset(const dynamic_bitset &bits);
+    dynamic_bitset(dynamic_bitset &&bits);
+
+#pragma push_macro("new")
+#undef new
 
     dynamic_bitset &operator =(dynamic_bitset &&bits) {
         if (_buffer) {
-            free(_buffer);
+            delete[] _buffer;
         }
-        _size = bits._size;
-        _maxSize = bits._maxSize;
-        _buffer = bits._buffer;
-        bits._size = 0;
-        bits._maxSize = 0;
-        bits._buffer = nullptr;
+        ::new(static_cast<void *>(this)) dynamic_bitset(std::move(bits));
         return *this;
     }
 
-    dynamic_bitset &operator =(const dynamic_bitset &bitset) {
-		setMaxSize(bitset._maxSize);
+#pragma pop_macro("new")
+
+    dynamic_bitset &operator =(const dynamic_bitset &bitset)
+    {
+		setMaxSize(bitset._maxSize); // frees _buffer
         setSize(bitset._size);
         memcpy(_buffer, bitset._buffer, ((_maxSize + 7) >> 3));
         return *this;
@@ -74,6 +73,10 @@ public:
         return *this;
     }
 	dynamic_bitset &operator =(const char * str) {
+		setString(str);
+		return *this;
+    }
+	dynamic_bitset &operator =(const String &str) {
 		setString(str);
 		return *this;
     }
@@ -103,12 +106,16 @@ public:
     }
 #endif
 
+    inline __attribute__((__always_inline__))
     void setString(const String &str) {
         setString(str.c_str(), str.length());
     }
+
+    inline __attribute__((__always_inline__))
 	void setString(const char *str) {
         setString(str, strlen(str));
     }
+
     void setString(const char *str, size_t len);
 
     void setSize(uint8_t size);
@@ -132,6 +139,89 @@ private:
     uint8_t _maxSize;
     uint8_t *_buffer;
 };
+
+
+inline dynamic_bitset::dynamic_bitset(uint8_t maxSize) :
+    dynamic_bitset((uint32_t)0, maxSize, maxSize)
+{
+}
+
+inline dynamic_bitset::dynamic_bitset(uint8_t size, uint8_t maxSize) :
+    dynamic_bitset((uint32_t)0, size, maxSize)
+{
+}
+
+inline dynamic_bitset::dynamic_bitset(uint32_t value, uint8_t size, uint8_t maxSize) :
+    _size(size),
+    _maxSize(maxSize),
+    _buffer(nullptr)
+{
+    if (_maxSize) {
+        _init();
+        setValue(value);
+    }
+}
+
+inline dynamic_bitset::dynamic_bitset(const dynamic_bitset &bits) :
+    _buffer(nullptr)
+{
+    *this = bits;
+}
+
+inline dynamic_bitset::dynamic_bitset(dynamic_bitset &&bits) :
+    _size(std::exchange(bits._size, 0)),
+    _maxSize(std::exchange(bits._maxSize, 0)),
+    _buffer(std::exchange(bits._buffer, nullptr))
+{
+}
+
+inline dynamic_bitset::dynamic_bitset(const char *fromBytes, uint8_t length, uint8_t size)
+{
+    setMaxSize(size);
+    setBytes((uint8_t *)fromBytes, length);
+}
+
+inline dynamic_bitset::~dynamic_bitset()
+{
+    if (_buffer) {
+        delete[] _buffer;
+    }
+}
+
+inline void dynamic_bitset::setBytes(uint8_t *bytes, uint8_t len)
+{
+    memcpy(_buffer, bytes, std::min(len, (uint8_t)((_maxSize + 7) >> 3)));
+}
+
+inline const uint8_t * dynamic_bitset::getBytes() const
+{
+    return _buffer;
+}
+
+inline void dynamic_bitset::setSize(uint8_t size)
+{
+    _size = size;
+    if (_size > _maxSize) {
+        _size = _maxSize;
+    }
+}
+
+inline const uint8_t dynamic_bitset::size() const
+{
+    return _size;
+}
+
+inline const uint8_t dynamic_bitset::getMaxSize() const
+{
+    return _maxSize;
+}
+
+
+inline dynamic_bitset::bit_helper::bit_helper(dynamic_bitset *bitset, uint8_t index)
+{
+    _index = index;
+    _bitset = bitset;
+}
 
 /*
 */
--- lib/KFCBaseLibrary/src/dyn_bitset.cpp
+++ lib/KFCBaseLibrary/src/dyn_bitset.cpp
@@ -6,61 +6,29 @@
 #include <PrintString.h>
 #include <algorithm>
 
-
-dynamic_bitset::dynamic_bitset() : _size(0), _maxSize(0), _buffer(nullptr) {
-}
-
-dynamic_bitset::dynamic_bitset(uint8_t maxSize) : dynamic_bitset((uint32_t)0, maxSize, maxSize) {
-}
-
-dynamic_bitset::dynamic_bitset(uint8_t size, uint8_t maxSize) : dynamic_bitset((uint32_t)0, size, maxSize) {
-}
-
-dynamic_bitset::dynamic_bitset(uint32_t value, uint8_t size, uint8_t maxSize) : _size(size), _maxSize(maxSize), _buffer(nullptr) {
-    if (_maxSize) {
-        _init();
-        setValue(value);
-    }
-}
-
-dynamic_bitset::dynamic_bitset(const char *fromBytes, uint8_t length, uint8_t size) {
-    setMaxSize(size);
-    setBytes((uint8_t *)fromBytes, length);
-}
-
-dynamic_bitset::~dynamic_bitset() {
-    if (_buffer) {
-        delete[] _buffer;
-    }
-}
-
-void dynamic_bitset::setBytes(uint8_t *bytes, uint8_t len) {
-    memcpy(_buffer, bytes, std::min(len, (uint8_t)((_maxSize + 7) >> 3)));
-}
-
-const uint8_t * dynamic_bitset::getBytes() const {
-    return _buffer;
-}
-
-String dynamic_bitset::getBytesAsString() const {
+String dynamic_bitset::getBytesAsString() const
+{
     PrintString output;
     for (int8_t i = ((_size + 7) >> 3) - 1; i >= 0; i--) {
-        output.printf("\\x%02x", ((unsigned)_buffer[i]) & 0xff);
+        output.printf_P(PSTR("\\x%02x"), static_cast<uint8_t>(_buffer[i]));
     }
     return output;
 }
 
-void dynamic_bitset::setValue(uint32_t value) {
+void dynamic_bitset::setValue(uint32_t value)
+{
     memcpy(_buffer, &value, std::min((int)sizeof(value), ((_maxSize + 7) >> 3)));
 }
 
-const uint32_t dynamic_bitset::getValue() const {
-    uint32_t value;
+const uint32_t dynamic_bitset::getValue() const
+{
+    uint32_t value = 0;
     memcpy(&value, _buffer, std::min((int)sizeof(value), ((_maxSize + 7) >> 3)));
     return value;
 }
 
-void dynamic_bitset::setString(const char * str, size_t len) {
+void dynamic_bitset::setString(const char * str, size_t len)
+{
     int offset = 0;
     if (len > _maxSize) {
         offset = len - _maxSize;
@@ -79,18 +47,8 @@ void dynamic_bitset::setString(const char * str, size_t len) {
     }
 }
 
-void dynamic_bitset::setSize(uint8_t size) {
-    _size = size;
-    if (_size > _maxSize) {
-        _size = _maxSize;
-    }
-}
-
-const uint8_t dynamic_bitset::size() const {
-    return _size;
-}
-
-void dynamic_bitset::setMaxSize(uint8_t maxSize) {
+void dynamic_bitset::setMaxSize(uint8_t maxSize)
+{
     _maxSize = maxSize;
     if (_buffer) {
         delete[] _buffer;
@@ -104,34 +62,21 @@ void dynamic_bitset::setMaxSize(uint8_t maxSize) {
     }
 }
 
-const uint8_t dynamic_bitset::getMaxSize() const {
-    return _maxSize;
-}
-
-void dynamic_bitset::fromString(const char *pattern, bool reverse) {
+void dynamic_bitset::fromString(const char *pattern, bool reverse)
+{
     uint8_t len = (uint8_t)strlen(pattern);
     setMaxSize(len);
     setSize(_maxSize);
-    uint8_t start, end, pos = 0;
-
-    if (reverse) {
-        start = len - 1;
-        end = 0;
-        while (start >= end) {
-            set(pos++, pattern[start] == '1');
-            start--;
-        }
-    } else {
-        start = 0;
-        end = len;
-        while (start < end) {
-            set(pos++, pattern[start] == '1');
-            start++;
-        }
+    auto endPtr = pattern + len;
+    uint8_t pos = reverse ? len - 1 : 0;
+    while (pattern < endPtr) {
+        set(pos, *pattern++ == '1');
+        reverse ? --pos : ++pos;
     }
 }
 
-String dynamic_bitset::toString() const {
+String dynamic_bitset::toString() const
+{
     String output;
     output.reserve(_size);
     for (int i = _size - 1; i >= 0; i--) {
@@ -140,7 +85,8 @@ String dynamic_bitset::toString() const {
     return output;
 }
 
-void dynamic_bitset::set(uint8_t pos, bool value) {  // zero index, first bit is 0
+void dynamic_bitset::set(uint8_t pos, bool value)  // zero index, first bit is 0
+{
     if (pos < _maxSize) {
         uint8_t mask = (1 << pos % 8);
         if (value) {
@@ -152,29 +98,30 @@ void dynamic_bitset::set(uint8_t pos, bool value) {  // zero index, first bit is
     }
 }
 
-bool dynamic_bitset::test(uint8_t pos) const {  // zero index, first bit is 0
+bool dynamic_bitset::test(uint8_t pos) const // zero index, first bit is 0
+{
     if (pos < _maxSize) {
         return _buffer[pos >> 3] & (1 << pos % 8);
     }
     return false;
 }
 
-String dynamic_bitset::generateCode(const char *variable, bool setBytes) const {
-
+String dynamic_bitset::generateCode(const char *variable, bool setBytes) const
+{
     PrintString code;
     uint8_t numBytes = ((_size + 7) >> 3);
     if (setBytes) {
-        code.printf("%s.setMaxSize(%d);\n", variable, _size);
+        code.printf_P(PSTR("%s.setMaxSize(%d);\n"), variable, _size);
         if (numBytes > 4) {
-            code.printf("%s.setBytes((uint8_t *)\"%s\", %d);\n", variable, getBytesAsString().c_str(), numBytes);
+            code.printf_P(PSTR("%s.setBytes((uint8_t *)\"%s\", %d);\n"), variable, getBytesAsString().c_str(), numBytes);
         } else {
-            code.printf("%s.setValue(0x%x);\n", variable, getValue());
+            code.printf_P(PSTR("%s.setValue(0x%x);\n"), variable, getValue());
         }
     } else {
         if (numBytes > 4) {
-            code.printf("dynamic_bitset %s(\"%s\", %d, %d);\n", variable, getBytesAsString().c_str(), numBytes, _size);
+            code.printf_P(PSTR("dynamic_bitset %s(\"%s\", %d, %d);\n"), variable, getBytesAsString().c_str(), numBytes, _size);
         } else {
-            code.printf("dynamic_bitset %s(%s0x%x, %d, %d);\n", variable, getValue() ? "" : "(uint32_t)", getValue(), _size, _size);
+            code.printf_P(PSTR("dynamic_bitset %s(%s0x%x, %d, %d);\n"), variable, getValue() ? PSTR("") : PSTR("(uint32_t)"), getValue(), _size, _size);
         }
     }
 
@@ -185,11 +132,6 @@ void dynamic_bitset::_init()
 {
     _size = std::min(_size, _maxSize);
     auto sz = ((_maxSize + 7) >> 3);
-    _buffer = new uint8_t[sz];
-    std::fill_n(_buffer, sz, 0);
+    _buffer = new uint8_t[sz]();
 }
 
- dynamic_bitset::bit_helper::bit_helper(dynamic_bitset *bitset, uint8_t index) {
-     _index = index;
-     _bitset = bitset;
- }

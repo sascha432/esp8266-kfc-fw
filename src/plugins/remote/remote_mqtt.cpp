20a0dbee753725aa3a6910cde489a4875ae93693
sascha <sascha_lammers@gmx.de>
2021-02-24 07:12:11 -0800

    removed custom auto discovery

--- src/plugins/remote/remote_mqtt.cpp
+++ src/plugins/remote/remote_mqtt.cpp
@@ -14,51 +14,17 @@
 
 using KFCConfigurationClasses::Plugins;
 
-
 static inline String get_button_name(uint8_t n)
 {
     return PrintString(F("button_%u"), n + 1);
 }
 
-static void add_subtype_type_payload(MQTT::AutoDiscovery *discovery, uint8_t button, const __FlashStringHelper *subType)
+static void add_subtype_type_payload(MQTT::AutoDiscovery::EntityPtr discovery, uint8_t button, const __FlashStringHelper *subType)
 {
     __LDBG_printf("discovery i=%u sub_type=%s", button, subType);
     auto name = get_button_name(button);
     discovery->addSubType(name);
     discovery->addPayloadAndType(name, subType);
-}
-
-String MqttRemote::getAutoDiscoveryTriggersTopic() const
-{
-    AutoDiscovery discovery;
-    discovery.create(ComponentType::DEVICE_AUTOMATION, F("+"), FormatType::TOPIC);
-    return discovery.getTopic();
-}
-
-void MqttRemote::publishAutoDiscovery()
-{
-    __LDBG_printf("auto discovery pending=%u", _discoveryPending);
-    if (!_discoveryPending) {
-        if (!AutoDiscoveryQueue::isUpdateScheduled()) {
-            __LDBG_printf("no update scheduled");
-            return;
-        }
-        auto client = MQTTClient::getClient();
-        if (client) {
-            // stop auto discovery if running
-            __LDBG_printf("auto discovery running=%u", (bool)client->getAutoDiscoveryQueue());
-            client->getAutoDiscoveryQueue().reset();
-
-            __LDBG_printf("requesting removal of=%s", getAutoDiscoveryTriggersTopic().c_str());
-            client->removeTopicsRequest(this, getAutoDiscoveryTriggersTopic(), [this](ComponentPtr component, MQTTClient *client, bool result) {
-                __LDBG_printf("callback component=%p client=%p result=%u", component, client, result);
-                _discoveryPending = false;
-                if (client && result) {
-                    client->publishAutoDiscovery(true);
-                }
-            });
-        }
-    }
 }
 
 MqttRemote::AutoDiscoveryPtr MqttRemote::nextAutoDiscovery(FormatType format, uint8_t num)

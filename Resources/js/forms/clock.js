88b17279684e04f4710818063320e61aa79daf92
sascha <sascha_lammers@gmx.de>
2021-07-10 19:51:50 -0700

    detection for ambient light sensor

--- Resources/js/forms/clock.js
+++ Resources/js/forms/clock.js
@@ -4,21 +4,23 @@
 
 $(function() {
 	if ($('#clock_settings,#led_matrix_settings').length) {
-        $('#dis_auto_br').off('click').on('click', function() {
-            $('#auto_br').val(-1);
-        });
-        function load_sensor_value() {
-            $.get('/ambient_light_sensor?SID=' + $.getSessionId(), function (value) {
-                value = parseInt(value);
-                if (value > 0) {
-                    $('#abr_sv').html('Sensor&nbsp;<strong>' + value + '</strong>');
-                }
-                window.setTimeout(load_sensor_value, 2000);
-            }).fail(function(jqXHR, textStatus, error) {
-                console.error('failed to load sensor value ', error);
-                window.setTimeout(load_sensor_value, 30000);
+        if ($('#auto_br').length) {
+            $('#dis_auto_br').off('click').on('click', function() {
+                    $('#auto_br').val(-1);
             });
+            function load_sensor_value() {
+                $.get('/ambient_light_sensor?SID=' + $.getSessionId(), function (value) {
+                    value = parseInt(value);
+                    if (value > 0) {
+                        $('#abr_sv').html('Sensor&nbsp;<strong>' + value + '</strong>');
+                    }
+                    window.setTimeout(load_sensor_value, 2000);
+                }).fail(function(jqXHR, textStatus, error) {
+                    console.error('failed to load sensor value ', error);
+                    window.setTimeout(load_sensor_value, 30000);
+                });
+            }
+            window.setTimeout(load_sensor_value, 2000);
         }
-        window.setTimeout(load_sensor_value, 2000);
     }
 });

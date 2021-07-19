0a82d32688d933ceab5d61d932569cf78ab160c5
sascha <sascha_lammers@gmx.de>
2021-07-19 00:06:36 -0700

    led matrix form

---
+++ Resources/js/forms/led-matrix.js
@@ -0,0 +1,23 @@
+/**
+ * Author: sascha_lammers@gmx.de
+ */
+
+if ($('#collapse-rainbow').length) {
+    var items = {
+        0: [ 'rb_mul', 'rb_incr', 'rb_min', 'rb_max', 'rb_sp', 'rb_cf', 'rb_mv', 'rb_cre', 'rb_cgr', 'rb_cbl' ],
+        1: [ 'rb_bpm', 'rb_hue' ]
+    }
+    $('#rb_mode').on('change', function() {
+        var value = parseInt($(this).val());
+        var enable = value == 0 ? 0 : 1;
+        var disable = value == 0 ? 1 : 0;
+        var list = items[enable];
+        for (var i = 0; i < list.length; i++) {
+            $('#' + list[i]).closest('.form-group').show();
+        }
+        list = items[disable];
+        for (var i = 0; i < list.length; i++) {
+            $('#' + list[i]).closest('.form-group').hide();
+        }
+    }).trigger('change');
+}

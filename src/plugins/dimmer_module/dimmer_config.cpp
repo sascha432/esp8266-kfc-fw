930b7c14363be81385b69f4fed81080cc3f9f16b
sascha <sascha_lammers@gmx.de>
2021-08-10 20:26:54 -0700

    improved response when releasing a button (dimmer plugin)

--- src/plugins/dimmer_module/dimmer_config.cpp
+++ src/plugins/dimmer_module/dimmer_config.cpp
@@ -22,41 +22,35 @@ namespace KFCConfigurationClasses {
                         #else
                             channel_mapping{ 0, 1, 2, 3 },
                         #endif
-                        level{},
-                        on_fadetime(12.5),
-                        off_fadetime(12.5),
-                    #else
-                        level{},
-                        on_fadetime(4.5),
-                        off_fadetime(4.5),
                     #endif
-                    lp_fadetime(3.5)
+                    level{},
                     #if IOT_DIMMER_MODULE_HAS_BUTTONS
-                        ,off_delay(kDefaultValueFor_off_delay),
+                        off_delay(kDefaultValueFor_off_delay),
                         off_delay_signal(kDefaultValueFor_off_delay_signal),
+                        pin_ch0_down(kDefaultValueFor_pin_ch0_down),
                         pin_ch0_down_inverted(kDefaultValueFor_pin_ch0_down_inverted),
                         pin_ch0_up(kDefaultValueFor_pin_ch0_up),
                         pin_ch0_up_inverted(kDefaultValueFor_pin_ch0_up_inverted),
                         single_click_time(kDefaultValueFor_single_click_time),
                         longpress_time(kDefaultValueFor_longpress_time),
                         min_brightness(kDefaultValueFor_min_brightness),
-                        pin_ch0_down(kDefaultValueFor_pin_ch0_down),
                         shortpress_steps(kDefaultValueFor_shortpress_steps),
                         longpress_max_brightness(kDefaultValueFor_longpress_max_brightness),
                         longpress_min_brightness(kDefaultValueFor_longpress_min_brightness),
                         max_brightness(kDefaultValueFor_max_brightness),
-                        shortpress_time(kDefaultValueFor_shortpress_time)
+                        shortpress_time(kDefaultValueFor_shortpress_time),
+                        lp_fadetime(kDefaultValueFor_lp_fadetime),
                     #endif
+                    on_fadetime(kDefaultValueFor_on_fadetime),
+                    off_fadetime(kDefaultValueFor_off_fadetime)
             {
             }
 
             void Dimmer::defaults()
             {
                 DimmerConfig_t cfg = {};
+                // error: cannot bind packed field
                 // std::fill(std::begin(cfg.level.to), std::end(cfg.level.to), IOT_DIMMER_MODULE_MAX_BRIGHTNESS);
-                // for(auto &value: cfg.level.to) {
-                //     value = IOT_DIMMER_MODULE_MAX_BRIGHTNESS;
-                // }
                 for(uint8_t i = 0; i < IOT_DIMMER_MODULE_CHANNELS; i++) {
                     cfg.level.to[i] = IOT_DIMMER_MODULE_MAX_BRIGHTNESS;
                 }
--- src/plugins/dimmer_module/dimmer_def.h
+++ src/plugins/dimmer_module/dimmer_def.h
@@ -5,140 +5,127 @@
 #pragma once
 
 #ifndef DEBUG_IOT_DIMMER_MODULE
-#define DEBUG_IOT_DIMMER_MODULE             1
+#    define DEBUG_IOT_DIMMER_MODULE 0
 #endif
 
 // number of channels
 #ifndef IOT_DIMMER_MODULE_CHANNELS
-#define IOT_DIMMER_MODULE_CHANNELS          1
+#    define IOT_DIMMER_MODULE_CHANNELS 1
 #endif
 
 #ifndef DIMMER_CHANNEL_COUNT
-#define DIMMER_CHANNEL_COUNT                IOT_DIMMER_MODULE_CHANNELS
+#    define DIMMER_CHANNEL_COUNT IOT_DIMMER_MODULE_CHANNELS
 #endif
 
 // enable or disable buttons
 #ifndef IOT_DIMMER_MODULE_HAS_BUTTONS
-#define IOT_DIMMER_MODULE_HAS_BUTTONS       0
+#    define IOT_DIMMER_MODULE_HAS_BUTTONS 0
 #endif
 
 // use UART instead of I2C
 #ifndef IOT_DIMMER_MODULE_INTERFACE_UART
-#define IOT_DIMMER_MODULE_INTERFACE_UART    0
+#    define IOT_DIMMER_MODULE_INTERFACE_UART 0
 #endif
 
 // max. brightness level
 #ifndef IOT_DIMMER_MODULE_MAX_BRIGHTNESS
-#define IOT_DIMMER_MODULE_MAX_BRIGHTNESS    8192
+#    define IOT_DIMMER_MODULE_MAX_BRIGHTNESS 8192
 #endif
 
 #ifndef DIMMER_MAX_LEVEL
-#define DIMMER_MAX_LEVEL                    IOT_DIMMER_MODULE_MAX_BRIGHTNESS
-#endif
-
-#ifndef IOT_DIMMER_MODULE_HOLD_REPEAT_TIME
-#define IOT_DIMMER_MODULE_HOLD_REPEAT_TIME  125
+#    define DIMMER_MAX_LEVEL IOT_DIMMER_MODULE_MAX_BRIGHTNESS
 #endif
 
 // UART only change baud rate of the Serial port to match the dimmer module
 #ifndef IOT_DIMMER_MODULE_BAUD_RATE
-#define IOT_DIMMER_MODULE_BAUD_RATE         57600
+#    define IOT_DIMMER_MODULE_BAUD_RATE 57600
 #endif
 
-// I2C only. SDA PIN
-#ifndef IOT_DIMMER_MODULE_INTERFACE_SDA
-#define IOT_DIMMER_MODULE_INTERFACE_SDA     D3
-#endif
-
-// I2C only. SCL PIN
-#ifndef IOT_DIMMER_MODULE_INTERFACE_SCL
-#define IOT_DIMMER_MODULE_INTERFACE_SCL     D5
-#endif
-
-// pins for dimmer buttons
+// default pins for dimmer buttons
+// the buttons can be configured in the webui as well as the state active low/high
 // each channel needs 2 buttons, up & down
 #ifndef IOT_DIMMER_MODULE_BUTTONS_PINS
-#define IOT_DIMMER_MODULE_BUTTONS_PINS      D6, D7
+#    define IOT_DIMMER_MODULE_BUTTONS_PINS 4, 13
+#endif
+
+// repeat rate for hold button
+#ifndef IOT_DIMMER_MODULE_HOLD_REPEAT_TIME
+#    define IOT_DIMMER_MODULE_HOLD_REPEAT_TIME 125
 #endif
 
 #ifndef IOT_DIMMER_MODULE_PINMODE
-#define IOT_DIMMER_MODULE_PINMODE           INPUT
-#endif
-
-#ifndef IOT_SWITCH_PRESSED_STATE
-#define IOT_SWITCH_PRESSED_STATE            PinMonitor::ActiveStateType::PRESSED_WHEN_LOW
+#    define IOT_DIMMER_MODULE_PINMODE INPUT
 #endif
 
 // support for RGB dimming
 // requires at least 3 channels
 #ifndef IOT_DIMMER_HAS_RGB
-#define IOT_DIMMER_HAS_RGB                  0
+#    define IOT_DIMMER_HAS_RGB 0
 #endif
 
 // support for RGB + white
 // requires at least 4 channels
 #ifndef IOT_DIMMER_HAS_RGBW
-#define IOT_DIMMER_HAS_RGBW                 0
+#    define IOT_DIMMER_HAS_RGBW 0
 #endif
 
 // support for color temperature
 // requires at least 2 channels
 #ifndef IOT_DIMMER_HAS_COLOR_TEMP
-#define IOT_DIMMER_HAS_COLOR_TEMP           0
+#    define IOT_DIMMER_HAS_COLOR_TEMP 0
 #endif
 
 // allow dimming of each channel even if it belongs to RGB or color temperature group
 #ifndef IOT_DIMMER_SINGLE_CHANNELS
-#define IOT_DIMMER_SINGLE_CHANNELS          1
+#    define IOT_DIMMER_SINGLE_CHANNELS 1
 #endif
 
 // adds a group switch for all channels to the title
 #ifndef IOT_DIMMER_GROUP_SWITCH
-#define IOT_DIMMER_GROUP_SWITCH             true
+#    define IOT_DIMMER_GROUP_SWITCH true
 #endif
 
 // add switch to lock channel groups
 #ifndef IOT_DIMMER_GROUP_LOCK
-#define IOT_DIMMER_GROUP_LOCK               false
+#    define IOT_DIMMER_GROUP_LOCK false
 #endif
 
 #if !IOT_DIMMER_HAS_RGB && IOT_DIMMER_HAS_RGBW
-#error requires IOT_DIMMER_HAS_RGB=1
+#    error requires IOT_DIMMER_HAS_RGB=1
 #endif
 
 #if !IOT_DIMMER_HAS_RGB && !IOT_DIMMER_HAS_COLOR_TEMP && !IOT_DIMMER_SINGLE_CHANNELS
-#error requires IOT_DIMMER_SINGLE_CHANNELS=1 unless RGB or Color temperature is used
+#    error requires IOT_DIMMER_SINGLE_CHANNELS=1 unless RGB or Color temperature is used
 #endif
 
 #ifndef IOT_DIMMER_PLUGIN_NAME
-#define IOT_DIMMER_PLUGIN_NAME              "dimmer"
+#    define IOT_DIMMER_PLUGIN_NAME "dimmer"
 #endif
 
 #ifndef IOT_DIMMER_PLUGIN_FRIENDLY_NAME
-#define IOT_DIMMER_PLUGIN_FRIENDLY_NAME     "Dimmer"
+#    define IOT_DIMMER_PLUGIN_FRIENDLY_NAME "Dimmer"
 #endif
 
-
 #ifndef IOT_DIMMER_TITLE
-#define IOT_DIMMER_TITLE                    "Dimmer"
+#    define IOT_DIMMER_TITLE "Dimmer"
 #endif
 
 #ifndef IOT_DIMMER_BRIGHTNESS_TITLE
-#define IOT_DIMMER_BRIGHTNESS_TITLE         "Brightness"
+#    define IOT_DIMMER_BRIGHTNESS_TITLE "Brightness"
 #endif
 
 #if !defined(IOT_DIMMER_CHANNELS_TITLE) && IOT_DIMMER_SINGLE_CHANNELS
-#define IOT_DIMMER_CHANNELS_TITLE           "Channels"
+#    define IOT_DIMMER_CHANNELS_TITLE "Channels"
 #endif
 
 #if !defined(IOT_DIMMER_WBRIGHTNESS_TITLE) && IOT_DIMMER_HAS_RGBW
-#define IOT_DIMMER_WBRIGHTNESS_TITLE        "White Brightness"
+#    define IOT_DIMMER_WBRIGHTNESS_TITLE "White Brightness"
 #endif
 
 #if !defined(IOT_DIMMER_RGB_TITLE) && IOT_DIMMER_HAS_RGBW
-#define IOT_DIMMER_RGB_TITLE                "Color"
+#    define IOT_DIMMER_RGB_TITLE "Color"
 #endif
 
 #if !defined(IOT_DIMMER_COLOR_TEMP_TITLE) && IOT_DIMMER_HAS_COLOR_TEMP
-#define IOT_DIMMER_COLOR_TEMP_TITLE         "Color Temperature"
+#    define IOT_DIMMER_COLOR_TEMP_TITLE "Color Temperature"
 #endif

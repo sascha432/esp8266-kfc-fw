c40ccd72c95bf9f6e2f8c62e521bb03712a5528d
sascha <sascha_lammers@gmx.de>
2022-02-12 17:25:59 -0800

    update auto discovery script

--- scripts/tools/kfcfw_discovery.py
+++ scripts/tools/kfcfw_discovery.py
@@ -1,5 +1,8 @@
 from zeroconf import ServiceBrowser, Zeroconf
 
+TYPE = "_kfcmdns._udp.local."
+FORMAT = "%-16.16s | %-22.22s | %s | %-16.16s | %-24.24s | %-19.19s | %s"
+
 class MyListener:
 
     def remove_service(self, zeroconf, type, name):
@@ -7,14 +10,40 @@ class MyListener:
 
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
-        print("Service %s added, service info: %s" % (name, info))
+        # print("Service %s added, service info: %s" % (name, info))
+        addresses = []
+        for addr in info.addresses:
+            addresses.append("%d.%d.%d.%d" % (addr[0], addr[1], addr[2], addr[3]))
+        name = name.replace('.' + TYPE, '')
+        try:
+            version = '%s Build %s ' % (info.properties[b'v'].decode('ascii') , info.properties[b'b'].decode('ascii'))
+        except:
+            version = '???'
+            version = str(info.properties)
+            pass
+        try:
+            title = info.properties[b't'].decode('ascii')
+        except:
+            title = '???'
+            pass
+        try:
+            device = info.properties[b'd'].decode('ascii')
+        except:
+            device = '???'
+            pass
+        print(FORMAT % (name, info.server, ("%- 5u" % (info.port)), ', '.join(addresses), title, version, device))
 
 
 zeroconf = Zeroconf()
 listener = MyListener()
 
-browser = ServiceBrowser(zeroconf, "_kfcmdns._udp.local.", listener)
+browser = ServiceBrowser(zeroconf, TYPE, listener)
+
 try:
-    input("Press enter to exit...\n\n")
+    print("Press enter to exit...")
+    title  = FORMAT % ('name', 'server', 'port ', 'addresses', 'device title', 'version', 'device')
+    print(title)
+    print('-' * len(title))
+    input("")
 finally:
     zeroconf.close()

7b8af55d81df4d1059386d0cdfd6abc88d88518e
Sascha Lammers <sascha_lammers@gmx.de>
2022-11-22 07:04:03 -0800

    dimmer firmware update

--- include/build.h
+++ include/build.h
@@ -2,6 +2,5 @@
  * Author: sascha_lammers@gmx.de
  */
 #pragma once
-#define __BUILD_NUMBER "11770"
-#define __BUILD_NUMBER_INT 11770
-
+#define __BUILD_NUMBER "11774"
+#define __BUILD_NUMBER_INT 11774

0413a40ce44c6cc65750991aff679fb4fa8e125d
Sascha Lammers <sascha_lammers@gmx.de>
2023-10-12 19:55:57 -0700

    Fixed issue with OpenWeatherMap forecast not being updated

--- include/build.h
+++ include/build.h
@@ -2,6 +2,5 @@
  * Author: sascha_lammers@gmx.de
  */
 #pragma once
-#define __BUILD_NUMBER "13919"
-#define __BUILD_NUMBER_INT 13919
-
+#define __BUILD_NUMBER "13923"
+#define __BUILD_NUMBER_INT 13923

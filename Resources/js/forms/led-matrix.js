c5b1f6295129efd502f65679b984dd3287d76cf7
Sascha Lammers <sascha_lammers@gmx.de>
2023-10-15 11:32:32 -0700

    added validator

--- Resources/js/forms/led-matrix.js
+++ Resources/js/forms/led-matrix.js
@@ -11,24 +11,22 @@
  $(function() {
     // clock.html
     if ($('#led-matrix-settings').length) {
-        $('#posix_tz').on('change', function() {
-            $('#pes').on('change', function(e) {
-                var self = $(this);
-                var valueStr = self.val();
-	            var value = parseInt(valueStr * 1.0);
-                if (valueStr === '') {
-                    e.preventDefault();
-                    return;
-                }
-                if (isNaN(value) || value <= 1) {
-                    window.setTimeout(function() {
-                        self.val(0).trigger('change');
-                    }, 1);
-                    e.preventDefault();
-                    return;
-                }
-                self.val(value);
-            });
+        $('#pes').on('change', function(e) {
+            var self = $(this);
+            var valueStr = self.val();
+            var value = parseInt(valueStr * 1.0);
+            if (valueStr === '') {
+                e.preventDefault();
+                return;
+            }
+            if (isNaN(value) || value <= 1) {
+                window.setTimeout(function() {
+                    self.val(0).trigger('change');
+                }, 1);
+                e.preventDefault();
+                return;
+            }
+            self.val(value);
         });
     }
 });

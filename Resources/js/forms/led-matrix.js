d7a174940f81d45a58dadfa02ecb9b1760610c3e
Sascha Lammers <sascha_lammers@gmx.de>
2023-10-15 09:38:16 -0700

    validator for LED groups

--- Resources/js/forms/led-matrix.js
+++ Resources/js/forms/led-matrix.js
@@ -7,3 +7,28 @@
         $.initFormLedMatrixFunc();
     }
  };
+
+ $(function() {
+    // clock.html
+    if ($('#led-matrix-settings').length) {
+        $('#posix_tz').on('change', function() {
+            $('#pes').on('change', function(e) {
+                var self = $(this);
+                var valueStr = self.val();
+	            var value = parseInt(valueStr * 1.0);
+                if (valueStr === '') {
+                    e.preventDefault();
+                    return;
+                }
+                if (isNaN(value) || value <= 1) {
+                    window.setTimeout(function() {
+                        self.val(0).trigger('change');
+                    }, 1);
+                    e.preventDefault();
+                    return;
+                }
+                self.val(value);
+            });
+        });
+    }
+});

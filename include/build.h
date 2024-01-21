86faaaac2fb5550c77ec9b0a34640bb3aaaa8b1f
Sascha Lammers <sascha_lammers@gmx.de>
2024-01-20 20:01:41 -0800

    Update environmental_sensor.ini and build.h files

--- include/build.h
+++ include/build.h
@@ -2,6 +2,6 @@
  * Author: sascha_lammers@gmx.de
  */
 #pragma once
-#define __BUILD_NUMBER "14387"
-#define __BUILD_NUMBER_INT 14387
+#define __BUILD_NUMBER "14444"
+#define __BUILD_NUMBER_INT 14444
 

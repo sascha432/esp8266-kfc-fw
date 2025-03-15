0f6ed1de41a9f5bda166d2517b832b38be4e8f63
Sascha Lammers <sascha_lammers@gmx.de>
2025-03-15 13:34:35 -0700

    some updates

--- include/build.h
+++ include/build.h
@@ -2,6 +2,6 @@
  * Author: sascha_lammers@gmx.de
  */
 #pragma once
-#define __BUILD_NUMBER "14612"
-#define __BUILD_NUMBER_INT 14612
+#define __BUILD_NUMBER "14695"
+#define __BUILD_NUMBER_INT 14695
 

844e01c2fb62182399f430b95e217f2bda8c08a7
Sascha Lammers <sascha_lammers@gmx.de>
2026-09-22 18:31:28 -0700

    renamed

--- src/plugins/clock/atmode.cpp
+++ src/plugins/clock/atmode.cpp
@@ -2,6 +2,8 @@
  * Author: sascha_lammers@gmx.de
  */
 
+ #if AT_MODE_SUPPORTED
+
 #include <Arduino_compat.h>
 #include <EventScheduler.h>
 #include <web_socket.h>
@@ -16,7 +18,6 @@
 #include <debug_helper_disable.h>
 #endif
 
-#if AT_MODE_SUPPORTED
 
 #include "at_mode.h"
 
